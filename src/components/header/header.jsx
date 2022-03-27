93c1a8b34f6d709ed5f39a75abba60a4d62413d9
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-03-27 14:50:25 +0100

    Adding a cart icon, css, prettier to single quotes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './header.styles.scss'
+import CartIcon from "../cart-icon/cart-icon";
 
 const Header = () => {
   return (
-    <nav className="nav-menu container">
-      <div className="logo">
-        <Link to="/">NOMAD</Link>
+    <nav className='nav-menu container'>
+      <div className='logo'>
+        <Link to='/'>NOMAD</Link>
       </div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/shop">SHOP</Link>
+          <Link to='/shop'>SHOP</Link>
         </li>
       </ul>
+      <ul>
+        <CartIcon />
+      </ul>
     </nav>
-  )
-}
+  );
+};
 export default Header
