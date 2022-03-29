03ab9ceaa5be7358ec06c2e291956e983eaa2891
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-03-29 08:54:06 +0100

    Shop page and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/home-page';
+import Shop from './components/pages/shop/shop';
 import NotFound from './components/not-found';
 import './App.scss';
 
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Routes>
         <Route exact path='/' element={<HomePage />} />
+        <Route path='/shop' element={<Shop />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
