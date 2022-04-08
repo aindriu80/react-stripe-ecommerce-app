9fbfc5ef941c0b86dd8ec78cd7d0177fdfed901b
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-08 11:45:07 +0100

    Adding Stripe to frontend to accept payments

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 import ProductsContextProvider from './context/products-context';
 import CartContextProvider from './context/cart-context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(`${process.env.PUBLISHABLE_KEY}`);
 
 ReactDOM.render(
   <BrowserRouter>
     <ProductsContextProvider>
       <CartContextProvider>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>,
