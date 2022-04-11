4ed83b466ff813e09c6db08393f63521623aa33f
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-11 08:00:16 +0100

    Success and Cancelled pages

---
+++ client/src/components/checkout/stripe-checkout/success.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../../shared/layout';
+import { CartContext } from '../../../context/cart-context';
+
+const Success = ({ history }) => {
+  const { clearCart } = useContext(CartContext);
+  const navigate = useNavigate();
+  useEffect(clearCart, []);
+
+  return (
+    <Layout>
+      <div className='checkout'>
+        <h1>Thank you for your order</h1>
+        <p>
+          We are currently processing your order and will send you an
+          confirmation email shortly
+        </p>
+        <button
+          className='button is-black nomad-btn submit'
+          onClick={() => navigate(`/shop`)}
+        >
+          Continue Shopping
+        </button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Success;
